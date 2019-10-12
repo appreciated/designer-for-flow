06252c44b52a997f3b90218d7df09c8a72d68cd0
Johannes Goebel <GoebelJohannes@gmx.net>
2019-10-12 14:29:18 +0200

    cleaning up app.js

--- src/main/electron/main.js
+++ src/main/electron/main.js
@@ -1,5 +1,5 @@
 const {
-    app, session, protocol, BrowserWindow, Menu, globalShortcut
+    app, session, protocol, BrowserWindow, Menu, globalShortcut, dialog
 } = require('electron');
 const remote = require('electron').remote;
 const path = require('path');
@@ -7,10 +7,52 @@ let mainWindow = null;
 let loading = null;
 let serverProcess = null;
 const gotTheLock = app.requestSingleInstanceLock();
-if (!gotTheLock) {
-    app.quit()
-}
-else {
+
+const port = 8080;
+
+const showDesigner = function (appUrl) {
+    mainWindow = new BrowserWindow({
+        title: 'Flow Designer'
+        , show: false
+        , width: 1200
+        , height: 800
+        , frame: true
+    });
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(appUrl);
+    mainWindow.once('ready-to-show', () => {
+        loading.hide();
+        mainWindow.show();
+    });
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+        app.quit();
+    });
+    mainWindow.on('close', function (e) {
+        if (serverProcess) {
+            var choice = require('electron').dialog.showMessageBox(this, {
+                type: 'question'
+                , buttons: ['Yes', 'No']
+                , title: 'Confirm'
+                , message: 'Are you sure you want to quit?'
+            });
+            if (choice == 1) {
+                e.preventDefault();
+            }
+        }
+    });
+};
+const awaitStartUp = function (appUrl, callback) {
+    const requestPromise = require('minimal-request-promise');
+    requestPromise.get(appUrl).then(function (response) {
+        callback();
+    }, function (response) {
+        setTimeout(function () {
+            awaitStartUp(appUrl, callback);
+        }, 200);
+    });
+};
+const focusSecondInstance = function () {
     app.on('second-instance', (event, commandLine, workingDirectory) => {
         if (mainWindow) {
             if (mainWindow.isMinimized()) {
@@ -19,6 +61,8 @@ else {
             mainWindow.focus();
         }
     })
+};
+const getJavaFile = function () {
     var fs = require("fs");
     var files = fs.readdirSync(app.getAppPath() + "/java/");
     var filename = null;
@@ -31,98 +75,71 @@ else {
     if (!filename) {
         throw new Error("There is no JAR file in ./java/ !");
     }
-    // Provide API for web application
-    global.callElectronUiApi = function (args) {
-        console.log('Electron called from web app with args "' + args + '"');
-        if (args) {
-            if (args[0] === 'exit') {
-                console.log('Kill server process');
-                const kill = require('tree-kill');
-                kill(serverProcess.pid, 'SIGTERM', function (err) {
-                    console.log('Server process killed');
-                    serverProcess = null;
-                    mainWindow.close();
-                });
-            }
-            if (args[0] === 'minimize') {
-                mainWindow.minimize();
-            }
-            if (args[0] === 'maximize') {
-                if (!mainWindow.isMaximized()) {
-                    mainWindow.maximize();
-                }
-                else {
-                    mainWindow.unmaximize();
-                }
-            }
-        }
-    };
+    return filename;
+};
+var isPortAvailable = function (callback) {
+    var server = require('net').createServer(function (socket) {
+        socket.write('Echo server\r\n');
+        socket.pipe(socket);
+    });
+
+    server.listen(port, '127.0.0.1');
+    server.on('error', function (e) {
+        callback(false);
+    });
+    server.on('listening', function (e) {
+        server.close();
+        callback(true);
+    });
+};
+const spawnServerProcess = function () {
+    var filename = getJavaFile();
+    platform = process.platform;
+    if (platform === 'win32') {
+        return require('child_process').spawn('java.exe', ['-jar', filename, '--logging.file=flow-designer.log'], {
+            cwd: app.getAppPath() + '/java/'
+        });
+    } else if (platform === 'darwin') {
+        return require('child_process').spawn(app.getAppPath() + '/java/' + filename);
+    } else {
+        throw new Error("Platform not supported");
+    }
+};
+const showLoadingScreen = function () {
+    loading = new BrowserWindow({
+        show: true
+        , frame: false
+        , width: 500
+        , height: 280
+    });
+    loading.loadURL('file://' + app.getAppPath() + '/loading.html');
+};
+
+if (!gotTheLock) {
+    app.quit()
+} else {
+    focusSecondInstance();
     app.on('window-all-closed', function () {
         app.quit();
     });
     app.on('ready', function () {
-        loading = new BrowserWindow({
-            show: true
-            , frame: false
-            , width: 500
-            , height: 280
-        });
-        loading.loadURL('file://' + app.getAppPath() + '/loading.html');
-        platform = process.platform;
-        if (platform === 'win32') {
-            serverProcess = require('child_process').spawn('java.exe', ['-jar', filename, '--logging.file=flow-designer.log'], {
-                cwd: app.getAppPath() + '/java/'
-            });
-        }
-        else if (platform === 'darwin') {
-            serverProcess = require('child_process').spawn(app.getAppPath() + '/java/' + filename);
-        }
-        console.log("Server PID: " + serverProcess.pid);
-        let appUrl = 'http://localhost:8080';
-        const openWindow = function () {
-            mainWindow = new BrowserWindow({
-                title: 'Flow Designer'
-                , show: false
-                , width: 1200
-                , height: 800
-                , frame: true
-            });
-            mainWindow.setMenu(null);
-            mainWindow.loadURL(appUrl);
-            mainWindow.once('ready-to-show', () => {
-                loading.hide();
-                mainWindow.show();
-            });
-            mainWindow.on('closed', function () {
-                mainWindow = null;
+        isPortAvailable(function (isPort) {
+            if (!isPort) {
+                dialog.showMessageBox(null, {
+                    type: 'error'
+                    , buttons: ['Ok']
+                    , title: 'Port not avaiable'
+                    , message: '"Designer for Flow" is not able to start if port ' + port + ' is not free.'
+                });
                 app.quit();
-            });
-            mainWindow.on('close', function (e) {
-                if (serverProcess) {
-                    var choice = require('electron').dialog.showMessageBox(this, {
-                        type: 'question'
-                        , buttons: ['Yes', 'No']
-                        , title: 'Confirm'
-                        , message: 'Are you sure you want to quit?'
-                    });
-                    if (choice == 1) {
-                        e.preventDefault();
-                    }
-                }
-            });
-        };
-        const startUp = function () {
-            const requestPromise = require('minimal-request-promise');
-            requestPromise.get(appUrl).then(function (response) {
-                console.log('Server started!');
-                openWindow();
-            }, function (response) {
-                console.log('Waiting for the server start...');
-                setTimeout(function () {
-                    startUp();
-                }, 200);
-            });
-        };
-        startUp();
+            } else {
+                showLoadingScreen();
+                spawnServerProcess();
+                var appUrl = "http://localhost:" + port;
+                awaitStartUp(appUrl, function () {
+                    showDesigner(appUrl);
+                });
+            }
+        });
     });
 }
