5be90b4c3be75af7d2e048ea36f7cc8d552a8bf4
Johannes Goebel <GoebelJohannes@gmx.net>
2020-09-16 20:05:50 +0200

    - added linux build

--- zip-darwin-windows.sh
+++ zip-darwin-windows.sh
@@ -2,3 +2,4 @@
 cd target/electron/
 zip -r ./designer-for-flow-win32-x64.zip ./designer-for-flow-win32-x64
 zip -r ./designer-for-flow-darwin-x64.zip ./designer-for-flow-darwin-x64
+zip -r ./designer-for-flow-linux-x64.zip ./designer-for-flow-linux-x64
